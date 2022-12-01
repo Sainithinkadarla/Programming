252ec20a89d16b70bdef8c07fa5849f916b1d446
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2022-12-01 18:08:46 +0000

    linked list - new insert methods

--- Data Structures/linked-list.c
+++ Data Structures/linked-list.c
@@ -28,50 +28,97 @@ struct node *insert()
 	printf("Enter the data to insert: ");
 	scanf("%d",&data);
 	struct node *t=getnode(data);
-	if(head)
+	if(head==NULL)
 		head=t;
 	else
 		tail->link=t;
 	tail=t;
+}
+struct node *insertbetween(int index)
+{
+	int counter=1;//counter
+
+	int data;//data to insert
+	printf("Enter the data to insert: ");
+	scanf("%d",&data);//reading data
+	struct node *t=getnode(data);//new node which is going to be inserted
+	
+	struct node *c=head;//counting node
+	while(c!=NULL)
+	{
+		c=c->link;
+		
+		if(counter==index-1)
+		{
+			printf("data=%d    counter=%d",c->data, counter);																	//TESTING
+			t->link=c->link;
+			c->link=t;
+		}
+		counter+=1;
+	}
+	printf("No. of nodes = %d",counter);
+}
+struct node *insertathead()
+{
+	int data;
+	printf("Enter the data to insert: ");
+	scanf("%d",&data);
+	struct node *t=getnode(data);
+	t->link=head;
+	head=t;
 }
 
 struct node *traverse()
 {
 	struct node *t;
 	t=head;
-	while(t)
+	int c=1;																		//TESTING
+	while(t!=NULL)
 	{
+		//printf("\ndata=%d\t   its address pointer=%p",t->data,t);
+		//printf("\tpointer= %p\t\n",t->link);
+		printf("\n%d ",c);
 		printf("%d\t",t->data);
 		t=t->link;
+		c+=1;
 	}
 }	
 
 struct node *delete()
 {
-	struct node *t=tail;
-	if (head)//terminates at head equals to NULL
+	struct node *t=head;
+	if (head!=NULL)//terminates at head equals to NULL
 	{
-		
+		head=head->link;
+		printf("\n%d deleted",t->data);
+		free(t);
 	}
 
 }
 
-struct node *insertbetween(int before, int after)
-{
 
-}
 void main()
 {
-	/*struct node *n1=getnode(10);
-	printf("data= %d    link=%p",n1->data, p);*/
 	int i=0;
-	insert();
-	insert();
-	insert();
-	insert();
-	
+	for(int i=0; i<4; i++)
+	{
+		insert();
+	}
 	
 	traverse();
-	printf("data= %d    link=%p",head->data, head);
-	printf("data= %d    link=%p",tail->data, tail);
+	delete();
+	traverse();
+	insertbetween(2);
+	printf("\n\n\n"); 												//TESTING	
+	traverse();
+	insertathead();
+	printf("\n\n\n"); 												//TESTING	
+	traverse();
+
+
+	/*struct node *n1=getnode(10);
+	printf("data= %d    link=%p",n1->data, p);
+
+	printf("data= %d    link=%p",head->data, head->link);
+	printf("data= %d    link=%p",tail->data, tail->link);*/
 }
