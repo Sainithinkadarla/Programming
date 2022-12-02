488adacc366e63321eb5986843522334d6b829e9
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2022-12-02 11:22:31 +0000

    comments

--- Java_LSK/Parameterized_constructor.java
+++ Java_LSK/Parameterized_constructor.java
@@ -1,13 +1,14 @@
+// Program to demonstrate Parameterized constructor
 public class Parameterized_constructor
 {
-    public int data;
-    Parameterized_constructor(int x)
+    public int data;//instance variable
+    Parameterized_constructor(int x)//parameterized constructor 
     {
-        data=x;
+        data=x;//assigning data 
     }
     public static void main(String[] args)
     {
-        Parameterized_constructor obj = new Parameterized_constructor(10);
-        System.out.println(obj.data);
+        Parameterized_constructor obj = new Parameterized_constructor(10);// calling constructor and passing value to assign in instance variable
+        System.out.println(obj.data); //printing data
     }
 }
