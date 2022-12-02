fc7740b999413df267e841fc4b865dac161911e2
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2022-12-02 08:12:29 +0000

    First commit

---
+++ Java_LSK/Operators.java
@@ -0,0 +1,8 @@
+//Use operators to const5ruct an expression
+public class Operators
+{
+    public static void main(String[] args)
+    {
+        System.out.println(10*10/(5+3)-1*4/2); //Expression with basic operators
+    }
+}
---
+++ Java_LSK/Variable_as_an_argument.java
@@ -0,0 +1,13 @@
+//Write a method that takes arguments and returns a value
+public class Variable_as_an_argument{
+ static void function(int x)
+    {
+        System.out.println(x);
+    }   
+
+public static void main(String[] args)
+{
+    
+    function(1);
+}
+}
