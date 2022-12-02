6e7aadf3c0953821bcc939fcce7c044e8ecdbffc
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2022-12-02 11:27:47 +0000

    comments

--- Java_LSK/Variable_as_an_argument.java
+++ Java_LSK/Variable_as_an_argument.java
@@ -1,6 +1,6 @@
 //Write a method that takes arguments and returns a value
 public class Variable_as_an_argument{
- static void function(int x)//method 
+ static void function(int x)//method declaration
     {
         System.out.println(x);
     }   
