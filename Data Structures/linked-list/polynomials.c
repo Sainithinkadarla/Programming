b2d8cdbb15b7594870ec14715d36aa0842f0a0b1
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2022-12-25 19:38:19 +0000

    end of the day

--- Data Structures/linked-list/polynomials.c
+++ Data Structures/linked-list/polynomials.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-#include<stdio.h>
+#include<stdlib.h>
 
+char expression[]="4x^3";
 
 struct term
 {
@@ -17,5 +18,37 @@ struct term *getnode(int num, int coef, int pow)
     t->num=num;
     t->coef=coef;
     t->pow=pow;
-    t->lin=NULL;
+    t->link=NULL;
+    return t;
+}
+
+void insert_to_nodes()
+{
+    int num=0, coef=0, pow=0;
+    for(int i=0; expression[i]; i++)
+    {
+        
+        printf("\n%c",expression[i]);
+    }
+}
+
+void justforunderstanding()
+{
+    int data=0, pow=0;
+    t=getnode(data,pow ,0);
+    printf("%d %d %d %p",t->num, t->coef, t->pow, t->link);
+}
+
+void evaluate()
+{
+    printf("\nI am nothing\n");
+}
+
+void main()
+{
+    
+    insert_to_nodes();
+    evaluate();
+    insert_to_nodes();
+    justforunderstanding();
 }
