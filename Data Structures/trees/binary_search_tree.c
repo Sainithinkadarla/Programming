c30c1e9464d4aaa7044243df1332845f7a2f4f6b
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2022-12-28 06:16:24 +0000

    binary search tree completed

--- Data Structures/trees/binary_search_tree.c
+++ Data Structures/trees/binary_search_tree.c
@@ -5,10 +5,11 @@ struct node
 {
     int data;
     struct node *left_child, *right_child;
-}*root=NULL, *t;
+}*root=NULL;
 
 struct node *getnode(int data)
 {
+    struct node *t;
     t=(struct node *)malloc(sizeof(struct node));
     t->data=data;
     t->left_child=t->right_child=NULL;
@@ -17,20 +18,42 @@ struct node *getnode(int data)
 
 struct node *insert(struct node *funroot, int data)
 {
-    if(!root)
+    struct node *temp=getnode(data);
+    if(!funroot)
     {
         funroot=temp;
     }
-    else if(data<=funroot->data)
+
+    else if(data<funroot->data)
     {
-        return insert(root->right_child,data);
+        funroot->left_child=insert(funroot->left_child,data);
     }
-    if(data>funroot->data)
+    else if(data>funroot->data)
     {
-        return insert(root->left_child,data);
+        funroot->right_child=insert(funroot->right_child,data);
     }
-    
-    return t;
+        
+    return funroot;
+}
+
+void in_order(struct node *root)
+{
+    if(root)
+    {
+        in_order(root->left_child);
+        printf("\t%d",root->data);
+        in_order(root->right_child);
+    }
+}
+
+void pre_order(struct node *root)
+{
+    if(root)
+    {
+        printf("\t%d",root->data);
+        pre_order(root->left_child);
+        pre_order(root->right_child);
+    }
 }
 
 void main()
@@ -43,4 +66,8 @@ void main()
     root=insert(root,11);
     root=insert(root,18);
     root=insert(root,27);
+    printf("\nPre order\n");
+    pre_order(root);
+    printf("\nin order\n");
+    in_order(root);
 }
