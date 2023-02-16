635627ef2c2104581d5714d41e34f72c366f9fa2
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2023-02-16 19:15:27 +0530

    syncing with lappy

--- Data Structures/trees/binary_search_tree.c
+++ Data Structures/trees/binary_search_tree.c
@@ -5,7 +5,7 @@ struct node
 {
     int data;
     struct node *left_child, *right_child;
-}*getnode, *t;
+}*root=NULL, *t;
 
 struct node *getnode(int data)
 {
@@ -15,20 +15,32 @@ struct node *getnode(int data)
     return t;
 }
 
-void insert()
+struct node *insert(struct node *funroot, int data)
 {
-    int data;
-    printf("\nEnter the data to insert: ");\
-    scanf("%d",&data);
-    t=getnode(data);
     if(!root)
     {
-        root=t;
+        funroot=temp;
     }
-    if(root->data)
+    else if(data<=funroot->data)
+    {
+        return insert(root->right_child,data);
+    }
+    if(data>funroot->data)
+    {
+        return insert(root->left_child,data);
+    }
+    
+    return t;
 }
 
 void main()
 {
-
+    root=insert(root,7);
+    root=insert(root,1);
+    root=insert(root,2);
+    root=insert(root,3);
+    root=insert(root,17);
+    root=insert(root,11);
+    root=insert(root,18);
+    root=insert(root,27);
 }
