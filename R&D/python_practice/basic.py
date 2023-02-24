11c7bd6eebeb23a1553de8d8e11435e7ae1aa4f0
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2023-02-24 17:57:47 +0530

    Initial push for laptop

---
+++ R&D/python_practice/basic.py
@@ -0,0 +1,6 @@
+a=1
+b=2
+print(a+b)
+print(type(a)) #To get type of the data type
+print(id(a)) #To get the address of the data 
+
---
+++ R&D/python_practice/datatypes.py
@@ -0,0 +1,41 @@
+#int 
+a= 11232
+
+#float
+b=122.2323
+
+# String
+s1="python"
+s2='nohtyp'
+
+#list
+l=[1,2,4,5,5,6,"theuejbfj"]
+
+#Sets
+set1={1,3,4,5}
+
+#Tuple
+tuple1=(1,23,5,5,6,6)
+
+#dictionary
+dict1={"sai":"nithin"}
+
+#Bool
+b1=True
+b2=False
+
+
+
+print(a)
+print(b)
+print(s1, s2)
+print(l)
+print(set1)
+print(tuple1)
+print(dict1)
+print(b1,b2)
+
+
+#Help
+help(dict)
+
