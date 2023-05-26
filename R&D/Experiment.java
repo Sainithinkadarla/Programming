30832e151824a5ec062309ed726a206daa20b614
Sainithinkadarla <93084968+Sainithinkadarla@users.noreply.github.com>
2023-05-26 17:53:55 +0530

    Experiment java file in R&D folder for test programs

---
+++ R&D/Experiment.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Experiment {
+    public static void main(String [] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c;
+        do 
+        {
+            c= (char) br.read();
+            System.out.println(c);
+        }while(c!='q');
+
+    }
+}
